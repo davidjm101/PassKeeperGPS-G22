6ae89d9307994507fc96130b5d878868f2e4c47d
Jorge Nogueira <kalymero1989@gmail.com>
2015-11-23 23:08:01 +0000

    Estrutras criadas
    Criei as estruturas dados1 e dados2 que renomeei respectivamente de
    cliente e conta. Foram criadas constantes adicionais que foram
    necessárias à criação das estruturas.

--- PassKeeper/PassKeeper/Estruturas_e_Prototipos.h
+++ PassKeeper/PassKeeper/Estruturas_e_Prototipos.h
@@ -1,3 +1,29 @@
+#define MAX_MK 16 // o numero maximo de caracteres para a master key, e para o nome da pasta
+#define MAX_USER 100 // maximo de caracteres para o username, nome de conta e pergunta de segurança
+#define MAX_RS 32 //  numero maximo de caracteres para a resposta a pergunta de segurança
+#define MAX_PS 100 // numero maximo de caracteres para a pergunta de segurança
+#define MAX_LOG 100 // numero maximo de caracteres para o login de uma conta
+#define MAX_NOME 50 // numero maximo de caracteres para o nome de uma conta
+#define MAX_PASS 16 // numero maximo de caracteres para a password de uma conta
+
+typedef struct dados1 cliente, *pcliente; //pcliente é um ponteiro para uma estrutura do tipo cliente  e cliente é outro nome para struct dados1
+struct dados1
+{
+	char username[MAX_USER];
+	char masterkey[MAX_MK];
+	char perg_seguranca[MAX_PS];
+	char resp_seguranca[MAX_RS];
+	pconta lista; // ponteiro para o inicio da lista ligada de estruturas do tipo conta
+};
+
+typedef struct dados2 conta, *pconta;
+struct dados2
+{
+	char nome[MAX_NOME];
+	char login[MAX_LOG];
+	char password[MAX_PASS];
+	pconta prox; // ponteiro para o proximo elemento da lista ligada
+};
 
 void menu();
 void titulo();
